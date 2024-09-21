33cb308bb4da51739670a50b7931af2c0b127f21
Sanan Baig <sanan@Sanans-Mac.local>
2024-09-21 15:22:01 +0500

    removed unused imports

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,7 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import { RouteManager } from "./navigator/main";
 import { BrowserRouter as Router } from "react-router-dom";
-import {
-  useQuery,
-  useMutation,
-  useQueryClient,
-  QueryClient,
-  QueryClientProvider,
-} from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
