58c6661781629135d818f1504326ec1b4eb93967
Sanan Baig <sanan@Sanans-Mac.local>
2024-09-21 15:15:04 +0500

    React query setup

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,28 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import { RouteManager } from "./navigator/main";
 import { BrowserRouter as Router } from "react-router-dom";
+import {
+  useQuery,
+  useMutation,
+  useQueryClient,
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Router>
         <Header />
         <Navbar />
         <RouteManager />
         <Footer />
       </Router>
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
